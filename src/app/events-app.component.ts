2930f120caeae7dd58694c11114ac04de5837b3e
rudrajyotibiswas <rudrajyoti.biswas@outlook.com>
2020-04-12 15:57:57 +0530

    static assets included

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-root',
+  template: `<h2>Hello World</h2>
+    <img src="assets/images/angularconnect-shield.png">
+  `
+})
+export class EventsAppComponent {
+  title = 'angular-fundamentals-pluralSight';
+}
