684e6fd4d94e26cff84e8fa391f76ecace462eb5
rudrajyotibiswas <rudrajyoti.biswas@outlook.com>
2020-04-12 23:45:05 +0530

    top navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'event-root',
   template: `
+  <nav-bar></nav-bar>
   <events-list></events-list>
   `
 })
