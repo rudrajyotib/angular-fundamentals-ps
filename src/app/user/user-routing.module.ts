a305966dfd00fc8fb9995276d4affe07916766fb
rudrajyotibiswas <rudrajyoti.biswas@outlook.com>
2020-04-19 22:00:08 +0530

    login form added

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { UserComponent } from './user.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path : 'profile', component:ProfileComponent},
+  {path : 'login', component : LoginComponent},
   { path: '**', redirectTo:'profile', pathMatch:'full'}];
 
 @NgModule({
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,21 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
 
-// const routes: Routes = [
-//   { path: '', component: ProfileComponent }
-// ];
 
 @NgModule({
-  declarations: [UserComponent, ProfileComponent],
+  declarations: [UserComponent, ProfileComponent, LoginComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
-    //RouterModule.forChild(routes)
+    UserRoutingModule,
+    FormsModule
   ]
 })
 export class UserModule { }
